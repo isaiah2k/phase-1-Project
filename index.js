2604d4fdc6db62dd7f7920cece271718535bc70d
Isaiah <zaymgmt707@gmail.com>
2024-10-08 16:13:50 -0700

    loads and renders habit list

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ const baseURL = 'http://localhost:3000/habits'
 
 const habitForm = document.getElementById('habit-form')
 const habitList = document.getElementById('habit-list')
+
+function loadHabits() {
+  fetch(baseURL)
+    .then(response => response.json())
+    .then(habits => {
+      renderHabits(habits)
+    })
+    .catch(error => console.error('Error loading habits:', error))
+}
+
+function renderHabits(habits) {
+  habits.forEach(habit => {
+    const habitItem = document.createElement('li')
+    habitItem.textContent = `${habit.name} (${habit.frequency})`
+
+    habitList.appendChild(habitItem)
+  })
+}
+
+loadHabits()
