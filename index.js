e53d127bed9ad72d6575190d37726f4a49eb90cb
Isaiah <zaymgmt707@gmail.com>
2024-10-08 20:59:38 -0700

    updates index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const habitList = document.getElementById('habit-list')
 
 function loadHabits() {
   fetch(baseURL)
-    .then(response => response.json())
+    .then(res => res.json())
     .then(habits => {
       renderHabits(habits)
     })
@@ -15,10 +15,36 @@ function loadHabits() {
 function renderHabits(habits) {
   habits.forEach(habit => {
     const habitItem = document.createElement('li')
-    habitItem.textContent = `${habit.name} (${habit.frequency})`
+    habitItem.textContent = `${habit.name} (${habit.frequency}) ${habit.progress}`
 
     habitList.appendChild(habitItem)
   })
 }
 
+function addHabit() {
+  const habitName = document.getElementById('habit-name').value
+  const habitFrequency = document.getElementById('habit-frequency').value
+
+  const newHabit = {
+    name: habitName,
+    progress: 0,
+    frequency: habitFrequency
+  }
+
+  fetch(baseURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newHabit)
+  })
+    .then(res => res.json())
+    .then(addedHabit => {
+      renderHabits([addedHabit])
+    })
+    .catch(error => console.error('Error adding habit:', error))
+}
 loadHabits()
+
+habitForm.addEventListener('submit', addHabit)
+
